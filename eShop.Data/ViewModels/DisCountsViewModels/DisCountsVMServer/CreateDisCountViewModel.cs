7ffc211299607073bcef44376794b19313d321ce
Hamed Shariati Mehr <nextlab.info@gmail.com>
2024-09-02 21:23:54 +0330

    changelog:1403-06-12: Completed Part-A PATCH3.7

--- eShop.Data/ViewModels/DisCountsViewModels/DisCountsVMServer/CreateDisCountViewModel.cs
+++ eShop.Data/ViewModels/DisCountsViewModels/DisCountsVMServer/CreateDisCountViewModel.cs
@@ -14,12 +14,21 @@ namespace eShop.Data.ViewModels.DisCountsViewModels.DisCountsVMServer
 		[MinLength(1, ErrorMessage = "{0} نمیتواند کمتر از {1} باشد")]
 		[MaxLength(1024, ErrorMessage = "{0} نمیتواند بیشتر از {1} باید")]
 		public string Code { get; set; }
+
+		[Display(Name = "تعداد کد")]
+		[Required(ErrorMessage = "{0} الزامیست.")]
 		public int? UserCount { get; set; }
 
 		[Display(Name = "فعال یا غیر کردن")]
 		[Required(ErrorMessage = "{0} الزامیست.")]
 		public bool IsActive { get; set; }
+
+		[Display(Name = "تاریخ شروع")]
+		[Required(ErrorMessage = "{0} الزامیست.")]
 		public string? StartDisCount { get; set; }
+
+		[Display(Name = "تاریخ پایان")]
+		[Required(ErrorMessage = "{0} الزامیست.")]
 		public string? EndDisCount { get; set; }
 	}
 }
